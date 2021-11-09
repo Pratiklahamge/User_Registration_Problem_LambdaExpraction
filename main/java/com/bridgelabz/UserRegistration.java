e484259f680549e98ca2e6842674a8ceeab37496
pratikl <plahamge@gmail.com>
2021-11-09 16:29:58 +0530

    Added: User need to enter a Valid Mobil no. formate

--- main/java/com/bridgelabz/UserRegistration.java
+++ main/java/com/bridgelabz/UserRegistration.java
@@ -3,7 +3,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Purpose  - As a User need to enter a valid First Name
  * @name     - Pratik lahamge
  * @version - 11.0
  */
@@ -32,6 +31,15 @@ public boolean email(String email) {
     String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
     Pattern pattern = Pattern.compile(regex);
     Matcher matcher = pattern.matcher(email);
+    return matcher.matches();
+}
+/*
+Created a method to validate Mobile Number Format
+*/
+public boolean phoneNumber(String phoneNumber) {
+    String regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(phoneNumber);
     return matcher.matches();
 }
 }
